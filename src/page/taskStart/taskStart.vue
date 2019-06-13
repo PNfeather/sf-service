2357de96ce282f43bd6bf9783acab4788828ba72
PNFeather <921192114@qq.com>
2019-06-13 17:53:01 +0800

    提交

--- src/page/taskStart/taskStart.vue
+++ src/page/taskStart/taskStart.vue
@@ -1,6 +1,35 @@
 <template>
-  <div name='taskStart'>
-    <titleBack title="返回任务列表" customBack @back="backTaskList"></titleBack>
+  <div name='taskStart' class="fillcontain">
+    <div class="title">
+      <titleBack title="返回任务列表" customBack @back="backTaskList"></titleBack>
+    </div>
+    <div class="template fillcontain">
+      <section class="functional">
+        <div class="title">今日化学作业模板列表</div>
+        <div class="btnGroup">
+          <a-button type="primary" class="funBtn" @click="checkTask">查看作业</a-button>
+          <a-button type="primary" class="funBtn" @click="goResource">资源库</a-button>
+          <a-button type="primary" class="funBtn" @click="leadImg">导入图片</a-button>
+        </div>
+      </section>
+      <section class="imgArea">
+        <div class="noContent fillcontain" v-if="!templateList.length">
+          <img src="~@IMG/noContent.png" alt="">
+          <p>请导入或者从资源库选择作业图片</p>
+          <p>（单次导入最多99张图片）</p>
+        </div>
+        <div class="item" v-for="(item, index) in templateList" :key="item">
+          <div class="delete">
+            <i class="iconfont iconClose"></i>
+          </div>
+          <img src="~@IMG/loginBack.png" alt="">
+          <p>第{{index + 1}}页</p>
+        </div>
+      </section>
+      <section class="submitBtn">
+        <a-button type="primary" class="submit" @click="submit">发布</a-button>
+      </section>
+    </div>
   </div>
 </template>
 
@@ -8,14 +37,9 @@
   import titleBack from '@C/titleBack.vue';
   export default {
     name: 'taskStart',
-    props: {
-      key: {
-        type: String,
-        default: ''
-      }
-    },
     data () {
       return {
+        templateList: new Array(20)
       };
     },
     created () {},
@@ -25,6 +49,18 @@
     methods: {
       backTaskList () {
         this.$router.push('missionList');
+      },
+      checkTask () {
+        console.log('查看作业');
+      },
+      goResource () {
+        console.log('资源库');
+      },
+      leadImg () {
+        console.log('导入图片');
+      },
+      submit () {
+        console.log('发布');
       }
     },
     components: {
@@ -33,5 +69,113 @@
   };
 </script>
 <style scoped lang="less">
-  [name = 'taskStart']{}
+  @import '~@/style/mixin';
+  [name = 'taskStart']{
+    display: flex;
+    flex-direction: column;
+    .title{
+      flex: 60px 0 0;
+    }
+    .template{
+      padding: 30px;
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      .functional{
+        flex: 30px 0 0;
+        .fj();
+        .title{
+          flex: 1;
+          font-size: 22px;
+          color: #333333;
+        }
+        .btnGroup{
+          flex: 1;
+          .fj(flex-end);
+          .funBtn{
+            margin-left: 14px;
+          }
+        }
+      }
+      .imgArea{
+        flex: 1;
+        overflow-y: scroll;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        padding: 10px 0;
+        .noContent{
+          .fac();
+          flex-direction: column;
+          img{
+            height: 185px;
+            width: auto;
+            margin-bottom: 30px;
+          }
+          p{
+            font-size: 16px;
+            color: #999999;
+            text-align: center;
+          }
+        }
+        .item{
+          margin-top: 20px;
+          flex: 190px 0 0;
+          height: 274px;
+          display: flex;
+          flex-direction: column;
+          border: 1px solid #E3EBF1;
+          border-radius: 4px;
+          overflow: hidden;
+          position: relative;
+          .delete{
+            position: absolute;
+            top: 5px;
+            right: 5px;
+            .wh(26px, 26px);
+            .fac();
+            border-radius: 100%;
+            &:after{
+              content: '';
+              position: absolute;
+              top: 2px;
+              left: 2px;
+              .wh(22px, 22px);
+              background-color: #fff;
+              border-radius: 100%;
+              z-index: 1;
+            }
+            .iconfont{
+              position: relative;
+              z-index: 2;
+              font-size: 26px;
+              color: #E46948;
+            }
+          }
+          img{
+            flex: 1;
+            width: 100%;
+          }
+          p{
+            flex: 50px 0 0;
+            line-height: 50px;
+            text-align: center;
+            font-size: 16px;
+            color: #333333;
+          }
+        }
+      }
+      .submitBtn{
+        width: 100%;
+        flex: 100px 0 0;
+        .fac();
+        .submit{
+          width: 240px;
+          height: 50px;
+          line-height: 50px;
+          font-size: 16px;
+        }
+      }
+    }
+  }
 </style>
