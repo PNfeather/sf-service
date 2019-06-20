d17c4a1e4a745459da4153ef76720abb9f0f65bd
PNFeather <921192114@qq.com>
2019-06-20 19:50:44 +0800

    提交

--- config/cs.env.js
+++ config/cs.env.js
@@ -3,5 +3,5 @@ const devEnv = require('./dev.env')
 module.exports = {
   NODE_ENV: '"production"',
   MOCK: 'false',
-  BASE_URL: '"http://cjapi.ecaiedu.com/api"'
+  BASE_URL: '"https://cjapi.ecaiedu.com/api"'
 }
