49d3a1b22ae6eef9aa7b45753c0f6482bafb449d
PNFeather <921192114@qq.com>
2019-06-26 09:35:06 +0800

    提交

--- src/api/tBook/index.js
+++ src/api/tBook/index.js
@@ -2,7 +2,7 @@ import {Http} from '@/plugins/http';
 
 // 新增模板书
 export const createBook = (data) => Http.post('/service/template-books', data);
-// 新增模板书
+// 获取模板书
 export const getBookTemplate = (bookId) => Http.get('/service/template-books/' + bookId);
 // 发布模板书
 export const releaseBook = (bookId) => Http.put('/service/template-books/' + bookId + '/release');
