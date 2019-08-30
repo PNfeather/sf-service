1cb3ecd62c23f26c28985bada6ab465e84fb7336
PNFeather <921192114@qq.com>
2019-08-30 17:44:16 +0800

    添加csdev环境

--- config/csdev.env.js
+++ config/csdev.env.js
@@ -4,6 +4,6 @@ module.exports = {
   NODE_ENV: '"production"',
   MOCK: 'false',
   // BASE_URL: '"http://10.0.2.252:8080/api"',
-  BASE_URL: '"http://47.112.96.161:8080/api"',
+  BASE_URL: '"https://devapi.ecaiedu.com/api"',
   CJIMGURL: '"https://cjfc-dev.oss-cn-shenzhen.aliyuncs.com/"'
 }
